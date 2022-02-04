f189b1d448e0785d492a045a1a6a8dd5da095a3f
Sannet0 <shupletsov.exceedteam@gmail.com>
2022-02-04 09:35:04 +0300

    event sourcing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,15 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getAllMessages(@Res() res: any): Promise<void> {
+    res.set('Connection', 'keep-alive');
+    res.set('Content-Type', 'text/event-stream');
+    res.set('Cache-Control', 'no-cache');
+    return this.appService.getMessage(res)
+  }
+
+  @Post()
+  addNewMessage(@Body() body: { message: string }): void  {
+    return this.appService.addNewMessage(body.message);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+import { nanoid } from 'nanoid';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private eventEmitter: EventEmitter2) {}
+
+  getMessage(res: any): void {
+    this.eventEmitter.on('message.created', (value) => {
+      res.write(`data: ${ JSON.stringify(value) } \n\n`);
+    })
+  }
+
+  addNewMessage(message: string): void {
+    const id = nanoid();
+    this.eventEmitter.emit(
+      'message.created',
+      { message, id }
+    );
   }
 }
