51638abf965a0a7a488b1715302a5593dfe15273
Sannet0 <shupletsov.exceedteam@gmail.com>
2022-02-04 12:26:07 +0300

    websocket

---
+++ src/app.gateway.ts
@@ -0,0 +1,39 @@
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer
+} from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { Server } from 'socket.io';
+
+@WebSocketGateway(3001, {
+  cors: {
+    origin: '*',
+  }
+})
+export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
+
+  private logger: Logger = new Logger('AppGateway');
+
+  afterInit(server: any) {
+    this.logger.log('Initialized!');
+  }
+
+  handleConnection(client: any, ...args): any {
+    this.logger.log('User Connect , id: ' + client.id);
+  }
+
+  handleDisconnect(client: any): any {
+    this.logger.log('User Disconnect, id: ' + client.id);
+  }
+
+  @SubscribeMessage('msgToServer')
+  handleMessage(client: Server, message: string): any {
+    this.server.emit('msgToClient', message);
+  }
+}
